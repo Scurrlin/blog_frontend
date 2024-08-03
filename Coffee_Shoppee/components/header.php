044b863d664b83da98701222dcf3c8b30175a99c
Sean Currlin <sean.currlin@gmail.com>
2024-08-03 10:08:12 -0400

    start header

---
+++ Coffee_Shoppee/components/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="flex">
+        <a href="home.php" class="logo"><img src="img/logo.jpg"></a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="view_products.php">Products</a>
+            <a href="order.php">Orders</a>
+            <a href="about.php">About Us</a>
+            <a href="contact.php">Contact Us</a>
+            
