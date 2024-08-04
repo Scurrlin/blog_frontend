5e9aaa28d6f1f05d9c457f974252b14b2690b1a1
Sean Currlin <sean.currlin@gmail.com>
2024-08-04 19:57:28 -0400

    specify connection socket

--- connection.php
+++ connection.php
@@ -4,9 +4,10 @@ $servername = "localhost";
 $username = "root"; // Default MAMP PRO username
 $password = "root"; // Default MAMP PRO password
 $dbname = "shop_db"; // Your database name
+$socket = "/Applications/MAMP/tmp/mysql/mysql.sock"; // Default MAMP PRO MySQL socket
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, null, $socket);
 
 // Check connection
 if ($conn->connect_error) {
